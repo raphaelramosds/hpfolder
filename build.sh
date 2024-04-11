fb123b29b96170be2f96f41ea2990f28afb2e536
Raphael Ramos <raphaelramos@geowellex.com>
2024-04-11 20:36:57 -0300

    feat: avoid cout

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-g++ *.cpp *.hpp -o hpfolder -lGL -lGLU -lglut -lboost_system -lboost_thread
+PROTEIN=BWBWWBBWBWWBWBBWWBWB
+
+g++ *.cpp *.hpp -o hpfolder -lGL -lGLU -lglut -lboost_system -lboost_thread -pg
+
+./hpfolder -e 100 -y -9 -p $PROTEIN -s 100 -m 0.05 -c 0.90 -g --ignore_rest
