9d2a4c1b0ca26f0ce4cc433f3d7c4c6db2d44c3e
Raphael Ramos <raphaelramos@geowellex.com>
2024-04-12 19:34:38 -0300

    chore: project build

--- build.sh
+++ build.sh
@@ -3,4 +3,6 @@ PROTEIN=BWBWWBBWBWWBWBBWWBWB
 
 g++ *.cpp *.hpp -o hpfolder -lGL -lGLU -lglut -lboost_system -lboost_thread -pg
 
-./hpfolder -e 100 -y -9 -p $PROTEIN -s 100 -m 0.05 -c 0.90 -g --ignore_rest
+./hpfolder -e 100000 -y -9 -p $PROTEIN -s 100 -m 0.05 -c 0.90 -g
+
+gprof ./hpfolder > log.txt
